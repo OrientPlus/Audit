1b2f38987e89c4de18fb1e6f13058778454f09d7
OrientPlus <gutrovr@gmail.com>
2021-04-15 16:52:24 +0300

    Pre-release version. Reads one extra character.

--- Audit3/Ex1/Ex1/ex1.c
+++ Audit3/Ex1/Ex1/ex1.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main()
 {
-
+    int n = 0, input, tmp;
+    printf("Enter N:");
+    scanf_s("%d", &n);
+    int* mass;
+    mass = (int*)malloc(n * sizeof(int));
+    printf("Enter element:");
+    for (int i = 0; i < n; i++)
+    {
+        scanf_s("%d", &mass[i]);
+    }
+    for (int i = 0; i < n - 1; i++) //Сортировка пузырьком
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (mass[j] > mass[j + 1])
+            {
+                tmp = mass[j];
+                mass[j] = mass[j + 1];
+                mass[j + 1] = tmp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) //вывод считанного массива
+    {
+        printf("  %d ", mass[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++) //Вывод уникальных элементов в квадрате
+    {
+        if (mass[i] != mass[i + 1])
+        {
+            input = mass[i] * mass[i];
+            printf("  UNIQ ELEMENT = %d \n", input);
+        }
+    }
+    free(mass);
+    printf("\n");
 	system("pause");
 	return 0;
 }
